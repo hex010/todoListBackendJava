a83d4f91c60fd379d780cb824deed3b9cb86cc7e
Robertas <robertas.nevoinas@stud.vilniustech.lt>
2023-04-20 22:54:49 +0300

    initial commit

---
+++ src/main/java/Todo/TodoListSpringBootJPA/TodoListSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package Todo.TodoListSpringBootJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListSpringBootJpaApplication.class, args);
+	}
+
+}
